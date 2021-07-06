0f9a13bd44624ff3d1f4e7af0a5e5e804977552f
Lury Rodrigues <lurylrodrigues@gmail.com>
2021-07-05 21:09:09 -0300

    create user authentication

--- src/middlewares/EnsureAdmin.ts
+++ src/middlewares/EnsureAdmin.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 
-export function ensureAdmin(req: Request, res: Response, next: NextFunction) {
+export async function ensureAdmin(req: Request, res: Response, next: NextFunction) {
     // verificar se o user é admin
-    const admin = true;
+    const { user_id } = req;
+    console.log(user_id);
+
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const { admin } = await usersRepository.findOne(user_id);
 
     if (admin) {
         return next();
